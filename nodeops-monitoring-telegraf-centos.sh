d7bf0e4baa0f8a055684df0f74751c1a21f7de7a
Supra-RaghulRajeshR <126448971+Supra-RaghulRajeshR@users.noreply.github.com>
2024-04-08 20:09:13 +0530

    Update nodeops-monitoring-telegraf-centos.sh

--- nodeops-monitoring-telegraf-centos.sh
+++ nodeops-monitoring-telegraf-centos.sh
@@ -61,7 +61,7 @@ hostname=$(hostname)
 uuid=$(uuidgen)
 uuid_2=$(uuidgen)
 title="Logs-$hostname-$public_ip"
-job=$hostname-$public_ip
+job=$hostname
 folder_uuid=$(uuidgen)
 folder_name="$hostname-$public_ip-Dashboard"
 metric_name="Metric-$hostname-$public_ip"
@@ -99,7 +99,7 @@ scrape_configs:
       - targets:
         - localhost
         labels:
-          job: $hostname-$public_ip
+          job: $hostname
           __path__: "$log_path"
 EOF
 
